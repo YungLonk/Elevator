07bc9567c1f168b4d3acd437ccf8d6f38f2858a8
YungLonk <amadeoc72@gmail.com>
2021-06-18 15:50:15 -0400

    Update FromFloor.py

--- FromFloor.py
+++ FromFloor.py
@@ -3,11 +3,13 @@ from Elevator import switchingFloors, execute
 from HelperFunctions import *
 
 # currentFloor: 1, fromFloor: 1, floorNeeded: 3, fromFloor2: 4, floorNeeded2: 2
-#     Order of travel: 1, 3, 4, 2
+#     Order of travel: 3, 4, 2
 # currentFloor: 3, fromFloor: 1, floorNeeded: 3, fromFloor2: 4, floorNeeded2: 2
 #     Order of travel: 1, 3, 4, 2
 # currentFloor: 2, fromFloor: 5, floorNeeded: 1, fromFloor2: 2, floorNeeded2: 4
 #     Order of travel: 2, 4, 5, 1
+# currentFloor: 3, fromFloor: 1, floorNeeded: 5, fromFloor2: 2, floorNeeded2: 5
+#     Order of travel: 2, 1, 5
 def efficient(currentFloor, fromFloor, fromFloor2, floorNeeded, floorNeeded2, fromFloor3=None, fromFloor4=None, floorNeeded3=None, floorNeeded4=None):
     noFloor3Or4 = (fromFloor3 == None) and (fromFloor4 == None)
     noFloor3Or4Needed = (floorNeeded3 == None) and (floorNeeded4 == None)
